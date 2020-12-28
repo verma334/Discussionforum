60e527d2c18ad92ae3696811691096d5cd6165b0
verma334 <74700262+verma334@users.noreply.github.com>
2020-12-28 16:57:01 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php 
+ function doDB() {
+     global $mysqli; 
+     //connect to server and select database; you may need it 
+     $mysqli = mysqli_connect("localhost", "root", "", "demo_forum");
+     
+      //if connection fails, stop script execution 
+      if (mysqli_connect_errno()) { 
+      printf("Connect failed: %s\n", mysqli_connect_error()); 
+       exit();
+      }
+    }
+?>
---
+++ demo_forum.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 27, 2020 at 07:34 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `demo_forum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_posts`
+--
+
+CREATE TABLE `forum_posts` (
+  `post_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `post_text` text NOT NULL,
+  `post_create_time` datetime NOT NULL,
+  `post_owner` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `forum_posts`
+--
+
+INSERT INTO `forum_posts` (`post_id`, `topic_id`, `post_text`, `post_create_time`, `post_owner`) VALUES
+(1, 1, 'jduisudw', '2020-12-27 23:02:20', 'verm334@gmail.com'),
+(2, 2, 'ok', '2020-12-27 23:04:20', 'abc@gmail.com'),
+(3, 3, 'ok', '2020-12-27 23:05:44', 'abc@gmail.com'),
+(4, 4, 'ok', '2020-12-27 23:24:14', 'abc@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_topics`
+--
+
+CREATE TABLE `forum_topics` (
+  `topic_id` int(11) NOT NULL,
+  `topic_title` varchar(150) NOT NULL,
+  `topic_create_time` datetime NOT NULL,
+  `topic_owner` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `forum_topics`
+--
+
+INSERT INTO `forum_topics` (`topic_id`, `topic_title`, `topic_create_time`, `topic_owner`) VALUES
+(1, 'abc', '2020-12-27 23:02:20', ''),
+(2, 'affvf', '2020-12-27 23:04:20', ''),
+(3, 'affvf', '2020-12-27 23:05:44', 'abc@gmail.com'),
+(4, 'affvf', '2020-12-27 23:24:14', 'abc@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `forum_posts`
+--
+ALTER TABLE `forum_posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `forum_topics`
+--
+ALTER TABLE `forum_topics`
+  ADD PRIMARY KEY (`topic_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `forum_posts`
+--
+ALTER TABLE `forum_posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `forum_topics`
+--
+ALTER TABLE `forum_topics`
+  MODIFY `topic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
